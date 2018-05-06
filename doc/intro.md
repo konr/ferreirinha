ed0d22de00bf516a418786d4b6c016a6a47dab31
Konrad Scorciapino <scorciapino@gmail.com>
2018-05-06 16:51:13 -0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ferreirinha
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ferreirinha/core_test.clj
@@ -0,0 +1,7 @@
+(ns ferreirinha.core-test
+  (:require [clojure.test :refer :all]
+            [ferreirinha.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
